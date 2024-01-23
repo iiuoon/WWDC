6f4b781fcfd622443ac53d7e6a2bda4918720677
Bibi-urssu <bibi.urssu@gmail.com>
2024-01-24 00:53:40 +0900

    env: code base setting

---
+++ WWDC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WWDC
+//
+//  Created by 박지윤 on 1/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
